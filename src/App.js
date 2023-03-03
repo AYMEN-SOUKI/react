e4107543fb1b7ec8a72544e3da6658594dfcb0c5
AYMEN-SOUKI <aymensouki10@yahoo.fr>
2023-03-03 15:17:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from './Navbar1';
+import Buttom from './Formulair';
+import Navbar1 from './Navbar1';
+
+import Formulair from './Formulair';
+
+function App() {
+  return (
+    <>
+   
+    <Navbar1></Navbar1>
+    <Formulair></Formulair>  
+
+    </>
+  );
+}
+
+export default App;
